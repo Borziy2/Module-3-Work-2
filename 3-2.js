800b612fca21824c73b1ae540fbf5033cc5b5864
Danil Doroshenko <doroshenkodanil272@gmail.com>
2024-11-01 16:30:13 +0700

    3/Work-2

---
+++ 3-2.js
@@ -0,0 +1,39 @@
+//Название статей
+
+let article1 = "JavaScript";
+let article2 = "Technology";
+
+console.log("Article 1 name:");
+console.log(article1);
+
+console.log("Article 2 name:");
+console.log(article2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Почему я выбрал JavaScript
+
+// let description1 = "Выбор упал на JavaScript по тому что это крутой язык программирования";
+// let description2 = "Еще не мало важный кретерий это универсальность данного языка";
+
+// console.log("description 1 name:");
+// console.log(description1);
+
+// console.log("description 2 name:");
+// console.log(description2);
---
+++ Workout/variables.js
@@ -0,0 +1,31 @@
+console.log("variables!");
+
+
+
+let b = "HI!";
+
+console.log(b);
+
+
+
+b = "123";
+
+console.log(b);
+
+
+const c = "Const!!!!";
+console.log(c);
+
+
+// c = "212"
+
+
+
+let B = 1
+
+console.log(B);
+
+
+let sum = 1 + 2;
+
+console.log(sum);
