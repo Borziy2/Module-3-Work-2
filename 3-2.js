91a9efda831c682a7f6a202afd6292ae08d187ae
Danil Doroshenko <doroshenkodanil272@gmail.com>
2024-11-01 16:32:52 +0700

    3/Work-2(modification in JS)

--- 3-2.js
+++ 3-2.js
@@ -1,39 +1,26 @@
-//Название статей
+//Почему я выбрал JavaScript
 
-let article1 = "JavaScript";
-let article2 = "Technology";
+let description1 = "Выбор упал на JavaScript по тому что это крутой язык программирования";
+let description2 = "Еще не мало важный кретерий это универсальность данного языка";
 
-console.log("Article 1 name:");
-console.log(article1);
+console.log("description 1 name:");
+console.log(description1);
 
-console.log("Article 2 name:");
-console.log(article2);
-
-
+console.log("description 2 name:");
+console.log(description2);
 
 
 
 
 
 
+// //Название статей
 
+// let article1 = "JavaScript";
+// let article2 = "Technology";
 
+// console.log("Article 1 name:");
+// console.log(article1);
 
-
-
-
-
-
-
-
-
-// //Почему я выбрал JavaScript
-
-// let description1 = "Выбор упал на JavaScript по тому что это крутой язык программирования";
-// let description2 = "Еще не мало важный кретерий это универсальность данного языка";
-
-// console.log("description 1 name:");
-// console.log(description1);
-
-// console.log("description 2 name:");
-// console.log(description2);
+// console.log("Article 2 name:");
+// console.log(article2);
