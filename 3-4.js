efd79e0c5f99f2a53c58fd5eeb055e7195739946
Danil Doroshenko <doroshenkodanil272@gmail.com>
2024-11-15 18:49:15 +0700

    Task 4 Lesson

---
+++ 3-4.js
@@ -0,0 +1,13 @@
+let articles = ['JavaScript', 'Techlogies', 'React'];
+
+articles.push('Angular');
+
+console.log(articles);
+
+let firstArticle = articles.shift()
+console.log(firstArticle);
+console.log(articles);
+
+
+let indexOfReact = articles.indexOf('React')
+console.log('React is artcle number', indexOfReact + 1 );
