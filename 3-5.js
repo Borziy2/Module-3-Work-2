ad2b298f3c1a2dac9fc3865204abeb5039e2f478
Danil Doroshenko <doroshenkodanil272@gmail.com>
2024-11-20 17:07:42 +0700

    Task 5 Lesson

---
+++ 3-5.js
@@ -0,0 +1,18 @@
+function logNewArticleName(articleName = 'new article'){
+    console.log('My new article name is', articleName);
+    
+}
+ 
+logNewArticleName('React');
+logNewArticleName();
+
+
+function exponent(x,y){
+    return x**y
+}
+ 
+const arrowExp = (x,y) =>{
+    return x ** y
+}
+
+const arrowExp2 = (x,y) => x ** y;
