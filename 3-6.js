2b212a27e52f8d6729c514c23167cc84b0c9e760
Danil Doroshenko <doroshenkodanil272@gmail.com>
2024-11-23 16:37:27 +0700

    Task 6 Lesson

---
+++ 3-6.js
@@ -0,0 +1,20 @@
+function User(name){
+    this.name = name;
+}
+
+function Article(name,text){
+    this.name = name;
+    this.text = text;
+
+}
+let user = new User("Alex"); 
+console.log(user);
+
+
+let article1 = new Article('React','React is awesome');
+let article2 = new Article('Angular','Angular is awesome too');
+
+
+user.article = [article1,article2];
+
+console.log(user);
