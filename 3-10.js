8e41af8ce90cbbd8aaca889f9646b0bac5547378
Danil Doroshenko <doroshenkodanil272@gmail.com>
2024-12-05 15:44:32 +0700

    Task 10 Lesson

---
+++ 3-10.js
@@ -0,0 +1,44 @@
+let user = {
+    name:'Bob',
+    age:37,
+    articles: [{name:'React'},{name:'javaScript'},{name:'Backend'}],
+};
+
+const getUserArticleCount = (user) =>{
+console.log(`User has ${user.articles.length} articles`);
+
+// switch (user.articles.length) {
+//     case 0:
+//     case 1:
+//         console.log('You need more!');
+//         break;
+//         case 2:
+//         case 3:
+//             console.log('Good start!');
+//             break;
+//     default:
+//         console.log('Keep going!');
+//         break;
+// }
+
+switch(true){
+    case user.articles.length < 2:
+    console.log('You need more!');
+     break;
+
+     case user.articles.length < 4:
+    console.log('Good start!');
+     break;
+     
+     default:
+        console.log('Keep going!');
+        break;
+}
+
+
+
+
+
+};
+
+getUserArticleCount(user);
